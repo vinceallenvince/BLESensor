19e292c0fbb562e1b4858817145f4943bcb96066
Vince Allen <vince@spotify.com>
2014-05-13 12:36:52 -0400

    Node app searches for port before connecting.

--- Web/BLESensor/app.js
+++ Web/BLESensor/app.js
@@ -3,79 +3,83 @@ var connect = require('connect'),
 		EventEmitter = require('events').EventEmitter,
     serialPort = require('serialport');
 
-/*serialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
-});*/
-
 var emitter = new EventEmitter();
+emitter.on('foundSerialPort', connectSerial);
 
+// setup web page
 connect(connect.static(__dirname + '/public')).listen(8080);
 
 io.sockets.on('connection', function(socket) {
 
   socket.emit('connected');
 
-  // control frame rate slider
   emitter.on('dataReceived', function(data) {
     socket.emit('dataReceived', data);
   });
 
 });
+
+var sp;
 var SerialPort = serialPort.SerialPort;
 
-//var mySerialPort = '/dev/tty.usbmodemfd121';
-var mySerialPort = '/dev/tty.usbmodem1421';
-
-var sp = new SerialPort(mySerialPort, {
-  baudrate: 57600
-  //parser: serialPort.parsers.readline('/n')
-}, false);
-
-sp.open(function() {
-
-  console.log('serial port open!');
-
-  sp.on('data', function(data) {
-  	console.log(data);
-
-  	if (data[0] == 38) { // '&'
-
-	  	var val = '';
-	  	var sign = data[1] == 45 ? '-' : '';
-
-	  	for (var i = 0; i < data.length; i++) {
-
-	  		if (data[i] == 46) {
-	  			val = val + '.';
-	  			continue;
-	  		}
-
-	  		if (data[i] != 38 && data[i] != 45) {
-	  			val = val + data[i];
-	  		}
-	  	};
-
-	  	val = parseFloat(sign + val);
-	  	console.log(val);
-
-	  	// when we receive data, publish it via the emitter
-			emitter.emit('dataReceived', {
-				val: val
-			});
-
-  	}
-
-  });
-
-  sp.on('close', function(data) {
-    console.log('close: ' + data);
-  });
-
-  sp.on('error', function(data) {
-    console.log('error: ' + data);
+serialPort.list(function (err, ports) {
+  ports.forEach(function(port) {
+    if (port.comName.search('usbmodem') !== -1) {
+      emitter.emit('foundSerialPort', {name: port.comName});
+    }
   });
 });
+
+function connectSerial(data) {
+  console.log('Will try to connect to: ' + data.name);
+
+  sp = new SerialPort(data.name, {
+    baudrate: 57600
+  }, false);
+
+  sp.open(function() {
+
+    console.log('serial port open!');
+
+    sp.on('data', function(data) {
+      console.log(data);
+
+      if (data[0] == 38) { // '&'
+
+        var val = '';
+        var sign = data[1] == 45 ? '-' : '';
+
+        for (var i = 0; i < data.length; i++) {
+
+          if (data[i] == 46) {
+            val = val + '.';
+            continue;
+          }
+
+          if (data[i] != 38 && data[i] != 45) {
+            val = val + data[i];
+          }
+        };
+
+        val = parseFloat(sign + val);
+        console.log(val);
+
+        // when we receive data, publish it via the emitter
+        emitter.emit('dataReceived', {
+          val: val
+        });
+
+      }
+
+    });
+
+    sp.on('close', function(data) {
+      console.log('close: ' + data);
+    });
+
+    sp.on('error', function(data) {
+      console.log('error: ' + data);
+    });
+
+  });
+}
